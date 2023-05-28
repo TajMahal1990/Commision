e1acf3ae8c1c66142a2071ef30d33c4a5e858eaf
Netology test <n89500554589@gmail.com>
2023-05-28 21:10:37 +0800

    Initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,19 @@
 fun main() {
-    println("First app!")
+    println("Программа денежных переводов:")
+    var procent = 0.75 // коммисия
+    var sum = 1000 // сумма перевода
+    var minimum = 35 // минимальная сумма перевода
+    var resultCom = sum / 100 * procent
+    var resultSum =sum - resultCom
+
+    println("Вы хотите перевести $sum рублей")
+
+    if (sum >= minimum) {
+        println("Комиссия составит $resultCom рублей")
+        println("Будет переведено с учетом комиссии $resultSum рублей")
+    } else {
+        println("Минимальная сумма для перевода 35 рублей")
+    }
 }
+
+
