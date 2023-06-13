455e9c97d0ff45e06f98deeccfcc4a13f515cca9
TajMahal1990 <130366681+TajMahal1990@users.noreply.github.com>
2023-06-14 05:07:55 +0800

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,19 @@
 fun main() {
     println("Программа перевода денег:")
     var procent = 0.75 // комиссия
-    var sum = 1000 // сумма которую надо перевести
-    var minimum = 35 // минимальная сумма перевода
-    var resultCom = sum / 100 * procent 
+    var sum = 10000000 // сумма которую надо перевести
+    var minimum = 35 // минимальная сумма комисии
+    var resultCom = sum / 100 * procent
     var resultSum =sum - resultCom
-
+    var perevod = sum - minimum
     println("Вы хотите перевести $sum рублей ")
 
-    if (sum >= minimum) {
+    if (((sum / 100) * procent) >= (minimum)) {
         println("Комиссия составит $resultCom рублей")
         println("Будет переведено с учетом комиссии $resultSum рублей")
     } else {
-        println("минимальная сумма для перевода 35 рублей")
+        println("Комиссия составит $minimum рублей")
+        println("Сумма перевода составит $perevod")
     }
 }
-
 
